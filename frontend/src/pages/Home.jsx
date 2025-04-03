f860fd399b83aeb66e152b2c711d5a4f71cd5569
JaneBM <janebmagai@gmail.com>
2025-04-03 17:50:24 -0600

    Added Pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import axios from axios
+
+
+
+const Home = () => {
+    // Api request using react
+    const [users,setUsers] = useState({})
+// Example Fetches all users
+    useEffect(()=> {
+        const fetchAllUsers = async() => {
+            try{
+                const res = await axios.get("https://localhost:8800/home")
+                console.log(res)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllUsers()
+    },[])
+
+    return (
+        <div>
+           Home          
+        </div>
+    )
+}
+
+export default Home
